2ca50e2176085c6fce404e071cfefb13756af9af
hewenjun-jk <hewenjun-jk@360jinrong.net>
2020-11-09 16:17:02 +0800

    style

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,32 @@
-const path = require('path');
-const apiRoutes = require('./mock');
-const resolve = (dir) => path.join(__dirname, dir);
+const path = require('path')
+const apiRoutes = require('./mock')
+const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
-    runtimeCompiler: true,
-    //允许对内部的 webpack 配置进行更细粒度的修改。
-    chainWebpack: (config) => {
-        //命名 
-        config.resolve.alias
-        .set('styles', resolve('src/assets/styles'))
-        .set('common', resolve('src/common'))
-        //打包文件带hash
-        config.output.filename('[name].[hash].js').end(); 
-    },
-    devServer: {
-        // open: true, 
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://127.0.0.1:8080',
-        //         ws: true,
-        //         changeOrigin: true,
-        //         pathRewrite: {
-        //             '^/api': '/mock'
-        //         }
-        //     },
-        // },
-        before(app) {
-            app.use('/api', apiRoutes)
-        }
+  runtimeCompiler: true,
+  // 允许对内部的 webpack 配置进行更细粒度的修改。
+  chainWebpack: (config) => {
+    // 命名
+    config.resolve.alias
+      .set('styles', resolve('src/assets/styles'))
+      .set('common', resolve('src/common'))
+    // 打包文件带hash
+    config.output.filename('[name].[hash].js').end()
+  },
+  devServer: {
+    // open: true,
+    // proxy: {
+    //     '/api': {
+    //         target: 'http://127.0.0.1:8080',
+    //         ws: true,
+    //         changeOrigin: true,
+    //         pathRewrite: {
+    //             '^/api': '/mock'
+    //         }
+    //     },
+    // },
+    before (app) {
+      app.use('/api', apiRoutes)
     }
+  }
 }
